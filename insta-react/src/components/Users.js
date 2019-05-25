3e8a35529cf3395e526f1dd5f3d3881570347c10
Богдан <bogdan@MacBook-Pro-Bogdan.local>
2019-05-25 20:52:34 +0300

    react-app

--- insta-react/src/components/Users.js
+++ insta-react/src/components/Users.js
@@ -9,23 +9,22 @@ export default function Users() {
                     alt="prince"
                     name="Harry_the_prince"/>
             <div className="users__block">
-            <User 
-                    src="https://www.loyatic.eu/wp-content/uploads/2017/11/iStock_000020004182Medium1.jpg"
-                    alt="prince"
-                    name="Harry_the_prince" min/>
-            <User 
-                    src="https://www.loyatic.eu/wp-content/uploads/2017/11/iStock_000020004182Medium1.jpg"
-                    alt="prince"
-                    name="Harry_the_prince" min/>
-            <User 
-                    src="https://www.loyatic.eu/wp-content/uploads/2017/11/iStock_000020004182Medium1.jpg"
-                    alt="prince"
-                    name="Harry_the_prince" min/>
-            <User 
-                    src="https://www.loyatic.eu/wp-content/uploads/2017/11/iStock_000020004182Medium1.jpg"
-                    alt="prince"
-                    name="Harry_the_prince" min/>
-                    
+                <User 
+                        src="https://www.loyatic.eu/wp-content/uploads/2017/11/iStock_000020004182Medium1.jpg"
+                        alt="prince"
+                        name="Harry_the_prince" min/>
+                <User 
+                        src="https://www.loyatic.eu/wp-content/uploads/2017/11/iStock_000020004182Medium1.jpg"
+                        alt="prince"
+                        name="Harry_the_prince" min/>
+                <User 
+                        src="https://www.loyatic.eu/wp-content/uploads/2017/11/iStock_000020004182Medium1.jpg"
+                        alt="prince"
+                        name="Harry_the_prince" min/>
+                <User 
+                        src="https://www.loyatic.eu/wp-content/uploads/2017/11/iStock_000020004182Medium1.jpg"
+                        alt="prince"
+                        name="Harry_the_prince" min/>
             </div>
         </div>
     )
